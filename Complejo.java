8cb0d2f1aece4eb044bae4a96904e05ae83d013d
oncekcm <59295554+oncekcm@users.noreply.github.com>
2024-03-07 14:44:10 -0400

    Update Complejo.java

--- Complejo.java
+++ Complejo.java
@@ -12,19 +12,19 @@ class principal{
         Complex y=new Complex(-5,3);
         Complex z=new Complex(0,0);
         z=x.adition(y);
-        System.out.println(z.toStr());
+        System.out.println("suma: " + z.toStr());
         
         // Realizar la multiplicación de los números complejos x e y
         Complex z2 = x.multiply(y);
-        System.out.println("Multiplicación: " + z2.toString());
+        System.out.println("Multiplicación: " + z2.toStr());
         
         // Realizar la división de los números complejos x e y
         Complex z3 = x.divide(y);
-        System.out.println("División: " + z3.toString());
+        System.out.println("División: " + z3.toStr());
         
         // Obtener el conjugado del número complejo x
         Complex conjugateX = x.conjugate();
-        System.out.println("Conjugado de x: " + conjugateX.toString());
+        System.out.println("Conjugado de x: " + conjugateX.toStr());
     }
 }
 
@@ -44,26 +44,25 @@ class Complex {
     // Método para realizar la multiplicación de dos números complejos
     Complex multiply(Complex y) {
         Complex z = new Complex(0, 0);
-        z.real = this.real * y.real - this.imag * y.imag;
-        z.imag = this.real * y.imag + this.imag * y.real;
+        z.real = this.real * y.real - this.img * y.img;
+        z.img = this.real * y.img + this.img * y.real;
         return z;
     }
     
     // Método para realizar la división de dos números complejos
     Complex divide(Complex y) {
         Complex z = new Complex(0, 0);
-        int denom = y.real * y.real + y.imag * y.imag;
-        z.real = (this.real * y.real + this.imag * y.imag) / denom;
-        z.imag = (this.imag * y.real - this.real * y.imag) / denom;
+        int denom = y.real * y.real + y.img * y.img;
+        z.real = (this.real * y.real + this.img * y.img) / denom;
+        z.img = (this.img * y.real - this.real * y.img) / denom;
         return z;
     }
     
     // Método para calcular el conjugado de un número complejo
     Complex conjugate() {
-        return new Complex(this.real, -this.imag);
+        return new Complex(this.real, -this.img);
     }
     String toStr(){
         return (String.valueOf(this.real)+ "+" + String.valueOf(this.img)+"i");
     }
 }
-
